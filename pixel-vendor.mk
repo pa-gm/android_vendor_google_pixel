1d3b90805cc2e1cd8bca59d607c8f65459f77e88
Gustavo Mendes <gusttavo.me@outlook.com>
2024-02-18 22:32:33 -0300

    pixel: Import AICore
    Change-Id: I9bc7ff047365458ca55004e37ca476befa524988
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

--- pixel-vendor.mk
+++ pixel-vendor.mk
@@ -106,6 +106,7 @@ PRODUCT_PACKAGES += \
     PixelWallpapers2023 \
     SoundPickerPrebuilt \
     WallpaperEmojiPrebuilt \
+    AICorePrebuilt \
     AiWallpapers \
     PixelLiveWallpaperPrebuilt \
     SettingsIntelligenceGooglePrebuilt \
